1cc572facc9a7d1c775d2567afdd008972952180
Lady Gaga <jessiezheqi.li@gmail.com>
2022-07-28 13:41:57 -0700

    Add failing test missing annotations

--- hello-circle/src/test/java/com/trilogyed/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/trilogyed/hellocircle/controller/HelloCircleControllerTest.java
@@ -1,7 +1,10 @@
 package com.trilogyed.hellocircle.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -10,6 +13,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.Assert.*;
 
+@WebMvcTest(HelloCircleController.class)
+@RunWith(SpringRunner.class)
 public class HelloCircleControllerTest {
     @Autowired
     MockMvc mockMvc;
